9fdcd51c17fcfab3154fa13029993b36f771a16d
kluerman <kristina.luermann@gmail.com>
2024-07-19 08:35:50 +0200

    Update main.py

--- app/app/main.py
+++ app/app/main.py
@@ -8,19 +8,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
-app.secret_key = 'xyz'
-UPLOAD_FOLDER = './uploads/'
-ALLOWED_EXTENSIONS = {'csv', 'log'}
-app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
